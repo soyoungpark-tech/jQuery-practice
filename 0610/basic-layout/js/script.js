7c82f6d00e06b2361f4f2daeddeae4c7ee6a9869
soyoungpark-tech <ps.young149@gmail.com>
2021-06-10 13:21:26 +0900

    making basic layout

---
+++ 0610/basic-layout/js/script.js
@@ -0,0 +1,29 @@
+'use stcict';
+
+// 기본세팅1
+// $(docment).ready(function(){
+
+// })
+// 기본 세팅2
+$(function(){
+    // header scroll evenet
+
+    // toggle ele
+    // gnb ele
+    var $toggle = $('.toggle'),
+        $gnb = $('.gnb');
+
+    $($toggle).click(function(){
+        $(this).toggleClass('active');
+        $($gnb).toggleClass('visible');
+    })
+    // gnb visible
+
+    // dimmed effect
+
+    // gnb__item window scroll 
+
+    // arrow up window scroll
+
+    // 
+})
