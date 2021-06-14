62b8f478b63407b16633766743beb21194a80c7b
soyoungpark-tech <ps.young149@gmail.com>
2021-06-15 02:06:06 +0900

    info delete

--- 0610/basic-layout/js/script.js
+++ 0610/basic-layout/js/script.js
@@ -1,44 +1,42 @@
-'use stcict';
+'use strict';
 
-// 기본세팅1
-// $(docment).ready(function(){
-
-// })
-// 기본 세팅2
+// 기본 세팅 1
+// $(document).ready(function(){
+    // })
+    
+// 기본 세팅 2
 $(function(){
 
     // wow js 초기화
     new WOW().init();
 
-    // header scroll evenet
+    // header scroll event
     var $header = $('#header');
     var $arrowUp = $('#arrowUp');
 
-    // number가 반환된다 = $를 붙이지 않는다 (개인스타일)
     $(window).scroll(function(){
-        // outerHeight는 border까지의 높이 포함
+        // number가 반환된다 = $를 붙이지 않는다 ( 개인 스타일 )
         var scrollTop = $(window).scrollTop();
+        /* outerHeight는 border까지의 높이 포함 */
         var headerHeight = $('#header').outerHeight();
         var aboutHeight = $('#about').outerHeight();
         // header가 감춰졌을때 fixed를 해서 따라오게 한다
         // 스크롤된 값이 헤더의 높이보다 커지면
 
-        console.log(scrollTop);
-        console.log(headerHeight);
-
-        if(scrollTop > headerHeight){
+        if( scrollTop > headerHeight ){
             $header.addClass('fixed');
         }else{
             $header.removeClass('fixed');
         }
 
-        // aboutSection을 지나치면 보여라 
+        // aboutSection을 지나치면 보여라
         // scrollTop 값이 about section 높이보다 커지면
         if( scrollTop > aboutHeight ){
             $arrowUp.addClass('visible');
         }else{
             $arrowUp.removeClass('visible');
-        }
+        }        
+
     })
 
     // toggle ele
@@ -52,23 +50,22 @@ $(function(){
         $(this).toggleClass('active');
         $($gnb).toggleClass('visible');
     })
-    // gnb visible
 
     // dimmed effect
     $dimmed.click(function(){
         $(this).removeClass('visible');
         $($gnb).removeClass('visible');
-        $toggle.toggleClass('active');
+        $toggle.removeClass('active');
     })
 
     // gnb__item window scroll 
     var $gnbMenu = $('.gnb__menu');
     $gnbMenu.click(function(){
+        // dataTarget #about, #skills, #contact
         var dataTarget = $(this).attr('data-target');
         // target은 클릭한 메뉴의 엘리먼트다
         var $target = $(dataTarget);
         var distance = $target.offset().top;
-
         $('html, body').animate({scrollTop: distance});
     })
 
